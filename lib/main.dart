6d684ed547dcbab0cf43c2d144562c5b8d7ad2ef
rjOwl <mostafayossef100@yahoo.com>
2021-07-03 03:38:24 +0200

    Edited customeAppBar calling

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return (MaterialApp(
       home: Scaffold(
-        appBar: CustomAppBar('', AppBar().preferredSize),
+        appBar: CustomAppBar('', AppBar().preferredSize, false),
         body: LoginScreen(),
       ),
     ));
